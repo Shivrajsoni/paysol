6f7c0d8bea83592657ae8ac7115bf8a3cc6febf4
Shivraj Soni <shivrajsoni09022005@gmail.com>
2025-06-17 13:03:35 +0530

    changed devnet to mainnet

--- src/components/walletConnector.tsx
+++ src/components/walletConnector.tsx
@@ -17,7 +17,7 @@ import "@solana/wallet-adapter-react-ui/styles.css";
 
 const WalletConnector = ({ children }: { children: React.ReactNode }) => {
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'
-  const network = WalletAdapterNetwork.Devnet;
+  const network = WalletAdapterNetwork.Mainnet;
 
   // You can also provide a custom RPC endpoint
   const endpoint = useMemo(() => {
